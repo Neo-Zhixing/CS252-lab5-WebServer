16431494a0530420a459405b14cad378c7ac4983
Zhixing Zhang <account@neoto.xin>
2020-04-19 09:27:44 -0400

    Commit

--- include/routes.hh
+++ include/routes.hh
@@ -13,7 +13,7 @@ void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock);
 void handle_htdocs(const HttpRequest& request, const Socket_t& sock);
 void handle_default(const HttpRequest& request, const Socket_t& sock);
 
-typedef std::function<HttpResponse(const HttpRequest&)> Route_Handler_t;
+typedef std::function<HttpResponse(const HttpRequest&, , const Socket_t&)> Route_Handler_t;
 
 typedef std::pair<const std::string, const Route_Handler_t> Route_t;
 
