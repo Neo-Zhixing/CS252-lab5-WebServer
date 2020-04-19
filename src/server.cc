7b8a7d0c7519fedb747edc6faae3a7620335490c
Zhixing Zhang <account@neoto.xin>
2020-04-19 05:34:56 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -80,7 +80,7 @@ void Server::get_request(const Socket_t& sock, HttpRequest& req) const {
         case 1:
           req.request_uri = token;
           std::size_t question_mark_location = token.find('?');
-          if (question_mark_location != std::string::npos) req.query = token.substr(token.find('?') + 1);
+          if (question_mark_location != std::string::npos) req.query = token.substr(question_mark_location+ 1);
           break;
         case 2: req.http_version = token;break;
         default: throw std::invalid_argument("Extra token on the first line"); 
