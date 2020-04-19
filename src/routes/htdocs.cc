8e64786e8c739b9299e447f79ff84718637fb3fa
Zhixing Zhang <account@neoto.xin>
2020-04-19 09:23:50 -0400

    Commit

--- src/routes/htdocs.cc
+++ src/routes/htdocs.cc
@@ -1,5 +1,7 @@
 #include "http_messages.hh"
 #include "socket.hh"
+#include <ifstream>
+
 // You may find implementing this function and using it in server.cc helpful
 
 void handle_htdocs(const HttpRequest& request, const Socket_t& sock) {
