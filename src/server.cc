20e1e8dc4a724a52351b4fc93dc4d88723cb3e2a
Zhixing Zhang <account@neoto.xin>
2020-04-19 08:03:01 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -65,7 +65,7 @@ void Server::handle(const Socket_t& sock) const {
   for (auto pair : route_map) {
     if (request.request_uri.compare(0, pair.first.length(), pair.first) == 0) {
       HttpResponse res = handle_htdocs(request);
-     // sock.write(res.to_string());
+      sock.write(res.to_string());
     }
   }
 }
