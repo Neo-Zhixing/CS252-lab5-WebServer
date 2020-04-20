1b4bfcabeb9ce4bf5fd415c5742792d05d57a566
Zhixing Zhang <account@neoto.xin>
2020-04-20 00:52:49 -0400

    Commit

--- src/server.cc
+++ src/server.cc
@@ -41,7 +41,7 @@ void Server::run_fork() const {
 
 void Server::run_thread() const {
   while (1) {
-    std::thread thread_obj([]() {
+    std::thread thread_obj([this]() {
       Socket_t sock = _acceptor.accept_connection();
       handle(sock);
     });
