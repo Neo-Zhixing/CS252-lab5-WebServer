6c6a2aad44c0a4e599e297eda799a1970c8276fb
Zhixing Zhang <account@neoto.xin>
2020-04-27 15:37:07 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -1,6 +1,6 @@
 #include "http_messages.hh"
 #include "socket.hh"
-
+#include <iostream>
 // You could implement your logic for handling /cgi-bin requests here
 
 void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
@@ -8,7 +8,7 @@ void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
   response.http_version = request.http_version;
   // TODO: Task 2.2
 
-  
+  std::cout << response.querystring << std::endl;
 
   sock->write(response.to_string());
 }
