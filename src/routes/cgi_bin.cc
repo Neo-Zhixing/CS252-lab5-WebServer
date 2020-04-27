94353eb351e50d96a2d8c17c5f1a9c2b868cb2bf
Zhixing Zhang <account@neoto.xin>
2020-04-27 15:42:57 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -4,9 +4,9 @@
 // You could implement your logic for handling /cgi-bin requests here
 
 void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
-  std::cout << "Trying.....to parse cgi_bin" << std::endl;
   HttpResponse response;
   response.http_version = request.http_version;
+  response.status_code = 200;
   // TODO: Task 2.2
 
   std::cout << request.query << std::endl;
