0556a192d82abfebc11acb0ee4416639910283be
Zhixing Zhang <account@neoto.xin>
2020-04-27 15:36:12 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -7,5 +7,8 @@ void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
   HttpResponse response;
   response.http_version = request.http_version;
   // TODO: Task 2.2
+
+  
+
   sock->write(response.to_string());
 }
