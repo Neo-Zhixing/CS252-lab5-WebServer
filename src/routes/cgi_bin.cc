9ffd3335ee1865fa5abbcd7d535cce14d8b061a2
Zhixing Zhang <account@neoto.xin>
2020-04-27 15:53:42 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -8,6 +8,19 @@ void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
   HttpResponse response;
   response.http_version = request.http_version;
   response.status_code = 200;
+
+  std::string original_querystring;
+  if (request.method.compare("GET") == 0) {
+    original_querystring = request.query;
+  } else {
+    original_querystring = request.body;
+  }
+
+  vector<string> querystrings; 
+  boost::split(querystrings, original_querystring, boost::is_any_of("&")); 
+
+
+
   std::cout << request.query << std::endl;
 
   sock->write(response.to_string());
