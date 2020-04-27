90627143cf2b9d49ad6f1e2a2f236ae58fda16a3
Zhixing Zhang <account@neoto.xin>
2020-04-27 15:49:16 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -1,14 +1,13 @@
 #include "http_messages.hh"
 #include "socket.hh"
 #include <iostream>
-// You could implement your logic for handling /cgi-bin requests here
+#include <map>
+#include <boost/algorithm/string.hpp> 
 
 void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
   HttpResponse response;
   response.http_version = request.http_version;
   response.status_code = 200;
-  // TODO: Task 2.2
-
   std::cout << request.query << std::endl;
 
   sock->write(response.to_string());
