fbe679b0ca0693e2e7a0d5125e90f02d7ce82ddb
Zhixing Zhang <account@neoto.xin>
2020-04-30 15:56:15 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -18,17 +18,11 @@ void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
   } else {
     original_querystring = request.message_body;
   }
-  int fdpipe[2];
-  pipe(fdpipe);
-
-  int writefd = fdpipe[1];
-  int readfd = fdpipe[0];
 
   int ret = fork();
   if (ret == 0) {
-    close(readfd);
     // Is child
-    dup2(writefd, 1); // Redirect stdout to the pipe
+    // dup2(writefd, 1); // Redirect stdout to the pipe
     setenv("REQUEST_METHOD", request.method.c_str(), 1);
     setenv("QUERY_STRING", original_querystring.c_str(), 1);
     std::string program_name = "http-root-dir" + request.request_uri;
@@ -44,17 +38,6 @@ void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
     std::cout << "Warning: something's wrong." << strerror(errno) << std::endl;
     _exit(1);
   } else {
-    // Is parent
-    close(writefd);
-    char buf[128];
-    while(true){
-      int len = read(readfd, buf, 128);
-      if (len <= 0) {
-        break;
-      }
-      std::cout << "Read " << buf << std::endl;
-      sock->write(buf, len);
-    }
     waitpid(ret, NULL, 0);
   }
 }
