b503fec3cb048fc64fbb3b90b4e581e3e5ea9faf
Zhixing Zhang <account@neoto.xin>
2020-04-30 16:33:55 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -34,8 +34,8 @@ void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
     setenv("REQUEST_METHOD", request.method.c_str(), 1);
     setenv("QUERY_STRING", original_querystring.c_str(), 1);
     int socketfd = sock->get_socket();
-    dup2(socketfd, 1); // Redirect stdout to the pipe
-    close(socketfd);
+    //dup2(socketfd, 1); // Redirect stdout to the pipe
+    //close(socketfd);
   
     char *argv[1];
     argv[0] = const_cast<char *>(program_name.c_str());
