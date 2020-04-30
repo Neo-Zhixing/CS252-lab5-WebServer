9656e302359e95cbb495397dbfa7c7fab421550c
Zhixing Zhang <account@neoto.xin>
2020-04-30 16:30:02 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -26,7 +26,7 @@ void handle_cgi_bin(const HttpRequest& request, const Socket_t& sock) {
     setenv("QUERY_STRING", original_querystring.c_str(), 1);
     int socketfd = sock->get_socket();
     dup2(socketfd, 1); // Redirect stdout to the pipe
-    close(fd);
+    close(socketfd);
     std::string program_name = "http-root-dir" + request.request_uri;
     // Remove everything after ?
     size_t index = program_name.find('?');
