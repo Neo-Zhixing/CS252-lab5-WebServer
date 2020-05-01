5dea0c867a5059c6c1f7fbd9937802b330e27406
Zhixing Zhang <account@neoto.xin>
2020-04-30 23:22:11 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -34,7 +34,7 @@ void handle_cgi_bin_fork(std::string& program_name, std::string& original_querys
 
 void handle_loadable(std::string& program_name, std::string& original_querystring, int socketfd, const HttpRequest& request) {
   std::cout << "Starting to load module" << std::endl;
-  auto absolute_path = realpath(program_name.c_str(), NULL);
+  char * absolute_path = realpath(program_name.c_str(), NULL);
   std::cout << "Were" << absolute_path << " about to load" << std::endl;
   return;
   void *dlo = dlopen(absolute_path, RTLD_LAZY);
