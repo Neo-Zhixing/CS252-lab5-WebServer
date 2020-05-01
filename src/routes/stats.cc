6a68ea09acb979903219e301361d40f09c8d5e20
Zhixing Zhang <account@neoto.xin>
2020-05-01 18:13:24 -0400

    Commit

--- src/routes/stats.cc
+++ src/routes/stats.cc
@@ -1,7 +1,6 @@
 #include "http_messages.hh"
 #include "socket.hh"
 #include "server.hh"
-#include <format>
 
 // If you use the recommended route handling scheme, you will probably want a default handler
 
@@ -9,16 +8,16 @@ void handle_stats(const HttpRequest& request, const Socket_t& sock) {
   HttpResponse response;
   response.http_version = request.http_version;
   response.status_code = 200;
-  response.message_body = std::format("\
+  response.message_body = "\
   <!DOCTYPE html>\
   <html>\
     <body>\
       <ul>\
         <li>Name: Zhixing Zhang</li>\
-        <li>Total requests served: {}</li>\
+        <li>Total requests served: " + std::string(total_requests)+ "</li>\
       </ul>\
     </body>\
-  </html>", total_requests);
+  </html>";
   sock->write(response.to_string());
 }
 
