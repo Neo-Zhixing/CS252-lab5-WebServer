4d92ccdd1c71dd73f94f56308b7cf407f53b10b3
Zhixing Zhang <account@neoto.xin>
2020-05-01 18:15:36 -0400

    Commit

--- src/routes/stats.cc
+++ src/routes/stats.cc
@@ -1,6 +1,5 @@
 #include "http_messages.hh"
 #include "socket.hh"
-#include "server.hh"
 
 // If you use the recommended route handling scheme, you will probably want a default handler
 
@@ -8,9 +7,8 @@ void handle_stats(const HttpRequest& request, const Socket_t& sock) {
   HttpResponse response;
   response.http_version = request.http_version;
   response.status_code = 200;
-  std::stringbuf buf;
-  buf << "\
-  <!DOCTYPE html>\
+  std::stringstream buf;
+  buf << "<!DOCTYPE html>\
   <html>\
     <body>\
       <ul>\
