e47251342372df0facccf0e520ce57de03711ff4
Zhixing Zhang <account@neoto.xin>
2020-04-30 23:18:57 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -6,6 +6,7 @@
 #include <boost/algorithm/string.hpp>
 #include <sys/wait.h>
 #include <dlfcn.h>
+#include <stdlib.h>
 
 void handle_cgi_bin_fork(std::string& program_name, std::string& original_querystring, int socketfd, const HttpRequest& request) {
   int ret = fork();
@@ -34,6 +35,8 @@ void handle_cgi_bin_fork(std::string& program_name, std::string& original_querys
 void handle_loadable(std::string& program_name, std::string& original_querystring, int socketfd, const HttpRequest& request) {
   std::cout << "Starting to load module" << std::endl;
   auto absolute_path = realpath(program_name.c_str(), NULL);
+  std::cout << absolute_path << " about to load" << std::endl;
+  return;
   void *dlo = dlopen(absolute_path, RTLD_LAZY);
   if (!dlo) {
     std::cout << "Can't load " << absolute_path << " with error " << dlerror() << std::endl;
