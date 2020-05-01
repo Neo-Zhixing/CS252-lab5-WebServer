0a50cea81e196b7289119fb40134084295d7c413
Zhixing Zhang <account@neoto.xin>
2020-04-30 22:24:23 -0400

    Commit

--- src/routes/cgi_bin.cc
+++ src/routes/cgi_bin.cc
@@ -32,6 +32,7 @@ void handle_cgi_bin_fork(std::string& program_name, std::string& original_querys
 }
 
 void handle_loadable(std::string& program_name, std::string& original_querystring, int socketfd, const HttpRequest& request) {
+  std::cout << "Starting to load module" << std::endl;
   void *dlo = dlopen(program_name.c_str(), RTLD_LAZY);
   if (!dlo) {
     std::cout << "Can't load " << program_name << " with error " << dlerror() << std::endl;
