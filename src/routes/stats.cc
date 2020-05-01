488944d1de192998b74baac362b8f791e4f0e400
Zhixing Zhang <account@neoto.xin>
2020-05-01 18:26:25 -0400

    Commit

--- src/routes/stats.cc
+++ src/routes/stats.cc
@@ -1,6 +1,7 @@
 #include "http_messages.hh"
 #include "socket.hh"
 #include "server.hh"
+#include <chrono>
 
 // If you use the recommended route handling scheme, you will probably want a default handler
 
@@ -14,6 +15,7 @@ void handle_stats(const HttpRequest& request, const Socket_t& sock) {
     <body>\
       <ul>\
         <li>Name: Zhixing Zhang</li>\
+        <li>Uptime: " << std::chrono::duration_cast<std::chrono::milliseconds>(startup_time - std::chrono::steady_clock::now()).count() << "\
         <li>Total requests served: " << total_requests << "</li>\
         <li>Max request time (ms): " << max_servetime << "</li>\
         <li>Min request time (ms): " << min_servetime << "</li>\
